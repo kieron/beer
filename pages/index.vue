41eac1da1b94a41facffdb1942af11e6f5b89d19
Kieron Boswell <kieron.boswell@asurafin.com>
2023-07-01 19:35:26 +0100

    add some more cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-<div class="flex space-2">
-  <Card />
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
   </div>
 </template>
